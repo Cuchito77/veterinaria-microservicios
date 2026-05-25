7a9747e654cd85a554d7f99b636015c22af50c95
Cuchito77 <kusikristal@gmail.com>
2026-05-24 23:21:20 -0400

    Anotación

--- ms-login/src/main/java/com/veterinaria/login/exception/RecursoNoEncontradoException.java
+++ ms-login/src/main/java/com/veterinaria/login/exception/RecursoNoEncontradoException.java
@@ -1,5 +1,7 @@
 package com.veterinaria.login.exception;
 
+//Sirve para retornar mensaje personaizado al buscar algo en la base de datos y no se encuentra
+
 public class RecursoNoEncontradoException extends RuntimeException {
     public RecursoNoEncontradoException(String mensaje) {
         super(mensaje);
